a4ef5b727cb0653177bbae8c536479a305d10202
Adrian Castueras <adrian.castueras@seer-technologies.com>
2015-02-18 19:54:56 +0800

    resize operation decode, moved encoder call to timer event

--- decoder.py
+++ decoder.py
@@ -22,3 +22,28 @@ class Decoder:
         obj.location.y += op['y']
         obj.location.z += op['z']
         
+    def rotate(self,op):
+        pass
+    
+    def resize(self,op):
+        obj = bpy.data.objects[op['target']]
+        if op['caxis_x'] == True:
+            obj.scale.x *= op['x']
+        if op['caxis_y'] == True:
+            obj.scale.y *= op['y']
+        if op['caxis_z'] == True:
+            obj.scale.z *= op['z']
+        if not self.check_constraint_axes(op):
+            obj.scale.x *= op['x']
+            obj.scale.y *= op['y']
+            obj.scale.z *= op['z']
+        
+        
+    def check_constraint_axes(self,op):
+        '''checks if an operation used a constraint axis'''
+        if op['caxis_x'] == False and op['caxis_y'] == False and op['caxis_z'] == False:
+            return False
+        
+        else:
+            return True
+        
